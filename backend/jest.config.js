02a964ab95523fd5162d116e59f2193d3d42d477
mrAJAY1 <thisismeajayprakash@gmail.com>
2024-01-10 05:18:45 +0530

    env and test command changes

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,8 +4,9 @@ module.exports = {
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
+  clearMocks: true,
+  collectCoverage: true,
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
 };
-
